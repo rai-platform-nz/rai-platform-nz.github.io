0d037aa6ba5477c639138becc13c0cbd7268b6cb
Andrew Lensen <andrewlensen@gmail.com>
2026-01-18 20:55:38 +1300

    Perf: Disabled modulePreload polyfill to fix forced reflow

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,14 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   build: {
+    modulePreload: {
+      polyfill: false
+    },
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
-        economics: resolve(__dirname, 'economics.html'),
         trust: resolve(__dirname, 'trust.html'),
+        economics: resolve(__dirname, 'economics.html'),
         maori: resolve(__dirname, 'maori.html'),
         education: resolve(__dirname, 'education.html'),
         team: resolve(__dirname, 'team.html'),
